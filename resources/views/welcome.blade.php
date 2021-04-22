7677b9ef8f2954fb037953c2070bd8179bcd9cb1
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-04-22 12:20:28 +0200

    Datenausgabe im Frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hallo {{ auth()->user()->name ?? "Unbekannt" }}
                 </div>
 
                 <div class="links">
