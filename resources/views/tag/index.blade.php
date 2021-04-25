890ee5aa0604fbf91a82991cd190497ee6835231
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-04-25 18:55:45 +0200

    Tag Style hinzugefügt

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -10,8 +10,9 @@
                 <div class="card-body">
                     <ul class="list-group">
                         @foreach($tags as $tag)
-                            <li class="list-group-item">{{ $tag->name }} 
-                            
+                            <li class="list-group-item">
+                                <span style="font-size: 130%;" class="mr-2 badge badge-{{$tag->style}}">{{ $tag->name }} </span>
+                                ({{$tag->style}})
                                 <a class="ml-2 btn btn-sm btn-outline-primary" href="/tag/{{ $tag->id }}/edit"><i class="fas fa-edit"></i> Bearbeiten</a>
                                 <form style="display: inline;" action="/tag/{{$tag->id}}" method="post">
                                     @csrf
