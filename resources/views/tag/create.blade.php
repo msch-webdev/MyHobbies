9cae792a1fc55f34b0d59d7579b457072b235231
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-04-25 18:08:06 +0200

    create.blade.php nachbearbeitet

--- resources/views/tag/create.blade.php
+++ resources/views/tag/create.blade.php
@@ -9,25 +9,28 @@
 
                 <div class="card-body">
                     
-                    <form action="/hobby" method="post">
+                    <form action="/tag" method="post">
                         @csrf
                         <div class="form-group">
                             <label for="name">Name</label>
                             <input type="text" class="form-control {{ $errors->has('name') ? 'border-danger' : ''}}" id="name" name="name" value="{{ old('name')}}">
                             <small class="form-text text-danger">{!! $errors->first('name') !!}</small>
                         </div>
-                        
+
                         <div class="form-group">
-                            <label for="beschreibung">Style</label>
-                            <textarea name="beschreibung" id="beschreibung" rows="5" class="form-control {{ $errors->has('name') ? 'border-danger' : ''}}">{{ old('style') }}</textarea>
+                            <label for="style">Style</label>
+
+                            <input type="text" class="form-control {{ $errors->has('style') ? 'border-danger' : ''}}" id="style" name="style" value="{{ old('style') ?? $tag->style }}">
                             <small class="form-text text-danger">{!! $errors->first('style') !!}</small>
                         </div>
+                        
+                        
                         
                         <input type="submit" class="btn btn-primary" value="absenden">
                     </form>
 
 
-                    <a class="btn btn-primary btn-sm mt-3 float-right" href="/hobby"> <i class="fas fa-arrow-circle-up"></i>Zurück</a>
+                    <a class="btn btn-primary btn-sm mt-3 float-right" href="/tag"> <i class="fas fa-arrow-circle-up"></i>Zurück</a>
                 </div>
             </div>
         </div>
