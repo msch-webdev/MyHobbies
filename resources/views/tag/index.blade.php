ace0dfbebcccb15531515569825c9e37a7e6dbb8
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-04-25 18:12:38 +0200

    index.blade.php Bearbeitet

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -5,15 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Alle Hobby´s</div>
+                <div class="card-header">Alle Tag´s</div>
 
                 <div class="card-body">
                     <ul class="list-group">
-                        @foreach($hobbies as $hobby)
-                            <li class="list-group-item">{{ $hobby->name }} 
-                                <a class="ml-2" href="/hobby/{{ $hobby->id }}">Detailansicht</a>
-                                <a class="ml-2 btn btn-sm btn-outline-primary" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Bearbeiten</a>
-                                <form style="display: inline;" action="/hobby/{{$hobby->id}}" method="post">
+                        @foreach($tags as $tag)
+                            <li class="list-group-item">{{ $tag->name }} 
+                            
+                                <a class="ml-2 btn btn-sm btn-outline-primary" href="/tag/{{ $tag->id }}/edit"><i class="fas fa-edit"></i> Bearbeiten</a>
+                                <form style="display: inline;" action="/tag/{{$tag->id}}" method="post">
                                     @csrf
                                     @method('DELETE')
                                     <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Löschen">
@@ -22,7 +22,7 @@
                             </li>
                         @endforeach
                     </ul>
-                    <a class="btn btn-success btn-sm mt-3" href="/hobby/create"> <i class="fas fa-plus-circle"></i> Neues Hobby Anlegen</a>
+                    <a class="btn btn-success btn-sm mt-3" href="/tag/create"> <i class="fas fa-plus-circle"></i> Neuen Tag Anlegen</a>
                 </div>
             </div>
         </div>
