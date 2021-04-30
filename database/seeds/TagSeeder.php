7cac556e0dbeffaae6f163be06b52f1a5d3df245
msch-webdev <82439727+msch-webdev@users.noreply.github.com>
2021-04-30 16:33:44 +0200

    User Forein und Seeder hinzugefügt

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
 
 class TagSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = [
+            'Sport' => 'primary', // blau
+            'Entspannung' => 'secondary', // grau-grau
+            'Fun' => 'warning', // gelb
+            'Natur' => 'success', // grün
+            'Inspiration' => 'light', // weiß-grau
+            'Freunde' => 'info', // türkis
+            'Liebe' => 'danger', // rot
+            'Interesse' => 'dark' // schwarz-weiss
+        ];
+
+        foreach ($tags as $key => $value) {
+            $tag = new Tag(
+                [
+                    'name' => $key,
+                    'style' => $value
+                ]
+            );
+            $tag->save();
+        }
     }
 }
